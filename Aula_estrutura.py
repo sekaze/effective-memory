2da9c1b34337a666299515db78a02d2d9a9eea13
sekaze <70239912+sekaze@users.noreply.github.com>
2020-08-25 19:09:14 -0300

    Add files via upload

---
+++ Aula_estrutura.py
@@ -0,0 +1,38 @@
+'''Lista de exercícios 18/08'''
+#Atividade um e dois
+a = 30
+b = 40
+c = 20
+print(a+b)
+print(a*b*c)
+
+#Atividade três
+a = 40
+b = 40
+print(a/b)
+
+#Atividade quatro
+a = 50
+b = 25
+print(a-b)
+
+
+#Atividade cinco
+nome = input('Digite seu nome: ')
+print('Como você está,',nome, '?')
+
+#Atividade sete
+num_str = input('Digite 123456: ')
+num_int = int(num_str)
+print( type(num_int),num_int)
+
+#Atividade oito
+a = 3
+b = 2
+print(int(a/b))
+
+a = 5.9874
+print(round(a,2))
+
+
+
